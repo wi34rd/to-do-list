ca6c1aec1d31364a9aca5488292a6965c8150ece
wi34rd <wi34rd@gmail.com>
2019-02-11 11:28:11 +0700

    Rewrites the element into React components

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const elt = (
-  <div>
-    <h1>To-Do List</h1>
-    <input placeholder="Search..." />
+const AppHeading = () => <h1>To-Do List</h1>;
 
+const SearchPanel = () => <input placeholder="Search" />;
+
+const ToDoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Make Awesome App</li>
     </ul>
-  </div>
-);
+  );
+};
 
-ReactDOM.render(elt, document.getElementById('app'));
+const App = () => {
+  return (
+    <div>
+      <AppHeading />
+      <SearchPanel />
+      <ToDoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
