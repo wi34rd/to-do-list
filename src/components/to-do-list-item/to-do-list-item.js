d81b5b831367ac83e9ee033c9adabc42234a647a
wi34rd <wi34rd@gmail.com>
2019-02-12 13:33:13 +0700

    Добавляет обработку клика на кнопке изменения важности задачи

--- src/components/to-do-list-item/to-do-list-item.js
+++ src/components/to-do-list-item/to-do-list-item.js
@@ -5,32 +5,42 @@ import './to-do-list-item.css';
 
 export default class ToDoListItem extends React.Component {
   state = {
-    done: false
+    isDone: false,
+    isImportant: this.props.isImportant
   };
 
   toDoListItemClick = () => {
-    this.setState({
-      done: !this.state.done
+    this.setState((state) => {
+      return {
+        isDone: !state.isDone
+      };
+    });
+  };
+
+  toDoListItemMarkImportant = () => {
+    this.setState((state) => {
+      return {
+        isImportant: !state.isImportant
+      };
     });
   };
 
   render() {
+    let text = this.props.text;
+    this.state.isDone && (text = <del>{text}</del>);
+    this.state.isImportant && (text = <strong>{text}</strong>);
+
     return (
       <div className="to-do-list-item d-flex">
-        {React.createElement(
-          this.props.isImportant ? 'strong': 'span',
-          {
-            className: 'to-do-list-item-text',
-            onClick: this.toDoListItemClick
-          },
-          this.state.done ? <del>{this.props.text}</del> : this.props.text
-        )}
+        <div onClick={this.toDoListItemClick}>
+          {text}
+        </div>
 
         <button type="button" className="btn btn-outline-danger btn-sm ml-auto">
           <span className="fa fa-trash-o"></span>
         </button>
 
-        <button type="button" className="btn btn-outline-success btn-sm ml-1">
+        <button type="button" className="btn btn-outline-success btn-sm ml-1" onClick={this.toDoListItemMarkImportant}>
           <span className="fa fa-exclamation"></span>
         </button>
       </div>
