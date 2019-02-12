8f9141ea750c5a0d7596640d62e857672c5e7238
wi34rd <wi34rd@gmail.com>
2019-02-12 08:33:01 +0700

    Переписывает компоненты TaskFilter и ToDoListItem из функций в классы

--- src/components/task-filter/task-filter.js
+++ src/components/task-filter/task-filter.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 
-const TaskFilter = () => {
-  return (
-    <div className="col-12 col-md-3 btn-group">
-      <button type="button" className="btn btn-info">All</button>
-      <button type="button" className="btn btn-outline-info">Active</button>
-      <button type="button" className="btn btn-outline-info">Done</button>
-    </div>
-  );
+export default class TaskFilter extends React.Component {
+  render() {
+    return (
+      <div className="col-12 col-md-3 btn-group">
+        <button type="button" className="btn btn-info">All</button>
+        <button type="button" className="btn btn-outline-info">Active</button>
+        <button type="button" className="btn btn-outline-info">Done</button>
+      </div>
+    );
+  }
 };
-
-export default TaskFilter;
--- src/components/to-do-list-item/to-do-list-item.js
+++ src/components/to-do-list-item/to-do-list-item.js
@@ -3,20 +3,22 @@ import React from 'react';
 import './to-do-list-item.css';
 
 
-const ToDoListItem = ({text, isImportant = false}) => {
-  return (
-    <div className="to-do-list-item d-flex">
-      {React.createElement(isImportant ? 'strong': 'span', null, text)}
+export default class ToDoListItem extends React.Component {
+  render() {
 
-      <button type="button" className="btn btn-outline-danger btn-sm ml-auto">
-        <span className="fa fa-trash-o"></span>
-      </button>
 
-      <button type="button" className="btn btn-outline-success btn-sm ml-1">
-        <span className="fa fa-exclamation"></span>
-      </button>
-    </div>
-  );
-};
+    return (
+      <div className="to-do-list-item d-flex">
+        {React.createElement(this.props.isImportant ? 'strong': 'span', null, this.props.text)}
 
-export default ToDoListItem;
+        <button type="button" className="btn btn-outline-danger btn-sm ml-auto">
+          <span className="fa fa-trash-o"></span>
+        </button>
+
+        <button type="button" className="btn btn-outline-success btn-sm ml-1">
+          <span className="fa fa-exclamation"></span>
+        </button>
+      </div>
+    );
+  };
+}
